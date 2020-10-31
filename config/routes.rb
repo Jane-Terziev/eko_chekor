677ee4a593aa451454c70bf0cc2135e7e704280f
Jane-Terziev <janeterziev@gmail.com>
2020-10-31 02:14:49 +0100

    Added landing page, added footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root controller: :healthcheck, action: :index
+  root controller: :roots, action: :index
   delete 'users/sessions', to: 'users/sessions#destroy'
 end
